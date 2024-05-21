6bb9f7a4a8d2f3842565b9e73718e0ed50d908a6
한창헌 <gksckdgjs334@gmail.com>
2024-05-21 17:09:42 +0900

    2

--- democh/src/main/java/com/japanch/democh/repository/BoardReopsitory.java
+++ democh/src/main/java/com/japanch/democh/repository/BoardReopsitory.java
@@ -1,5 +1,6 @@
 package com.japanch.democh.repository;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.japanch.democh.dto.BoardDTO;
@@ -7,11 +8,13 @@ import com.japanch.democh.dto.BoardDTO;
 import lombok.RequiredArgsConstructor;
 
 @Repository
+@RequiredArgsConstructor
 public class BoardReopsitory {
 
-    public final SqlSessionTemplate
+    public final SqlSessionTemplate sql;
     
     public void save(BoardDTO boardDTO) {
+        sql.insert("board.save", boardDTO);
     }
 
 }
