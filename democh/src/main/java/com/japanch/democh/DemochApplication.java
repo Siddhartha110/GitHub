953595ff247de9ce30a81cd5d86c25e42c3826bc
한창헌 <gksckdgjs334@gmail.com>
2024-05-21 09:45:32 +0900

    firstcommit

---
+++ democh/src/main/java/com/japanch/democh/DemochApplication.java
@@ -0,0 +1,13 @@
+package com.japanch.democh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemochApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemochApplication.class, args);
+	}
+
+}
